c02f459aa2aa864dad68b0c972483557ee726751
Ернар Пупкин <gamerhey2@gmail.com>
2024-12-14 23:41:02 +0500

    Task4

---
+++ Tyuiu.FilimonchukED.Sprint3.Task4.V2.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.FilimonchukED.Sprint3.Task4.V2.Lib;
+namespace Tyuiu.FilimonchukED.Sprint3.Task4.V2.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int strt = -5;
+            int stp = 5;
+            double res = ds.Calculate(strt, stp);
+            double wait = 0.104;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.FilimonchukED.Sprint3.Task4.V2/Program.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.FilimonchukED.Sprint3.Task4.V2.Lib;
+namespace Tyuiu.FilimonchukED.Sprint3.Task4.V2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнил: Филимончук Е. Д. | РППб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Использование операторов continue и break в циклах                *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #2                                                              *");
+            Console.WriteLine("* Выполнил: Филимончук Егор Дмитриевич | РППб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* На отрезке, где x принимает значения от -5 до 5,                        *");
+            Console.WriteLine("* вычислить значение функции y=cos(x)/x                                   *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("начальное число -5, последнее 5");
+            int strt = -5, stp = 5;
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Ответ = " + ds.Calculate(strt,stp));
+        }
+    }
+}
