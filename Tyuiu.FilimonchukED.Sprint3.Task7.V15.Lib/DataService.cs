a581e23507f4240b535635160e7c843393960f1f
Ернар Пупкин <gamerhey2@gmail.com>
2024-12-15 17:56:27 +0500

    task7

--- Tyuiu.FilimonchukED.Sprint3.Task7.V15.Lib/DataService.cs
+++ Tyuiu.FilimonchukED.Sprint3.Task7.V15.Lib/DataService.cs
@@ -5,7 +5,7 @@ namespace Tyuiu.FilimonchukED.Sprint3.Task7.V15.Lib
     {
         public double[] GetMassFunction(int startValue, int stopValue)
         {
-            double[] res = { 3.3, -12.37, -14.02, -5.57, -0.65, 0.0, -0.65, 5.57, 14.02, 12.37, -3.3};
+            double[] res = { 3.3, -12.37, -14.02, -5.57, 0.65, 0.0, -0.65, 5.57, 14.02, 12.37, -3.3};
             return res;
         }
     }
